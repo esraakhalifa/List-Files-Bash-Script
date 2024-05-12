d2349dc29a94110410ced27ccc1e1f2f4fd4a412
Esraa M. Khalifa <esraakhalifa5@gmail.com>
2024-05-12 15:44:20 +0300

    Add list_files.sh script

---
+++ list_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is a Bash script to execute the 'Is' command
+
+# Print a message
+echo "Executing 'Is' command to list files and directories:"
+
+# Execute the 'Is' command
+Is
