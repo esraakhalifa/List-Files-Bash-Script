911cc72d12eb66c0d7d362fefd279bff028257a2
Esraa M. Khalifa <esraakhalifa5@gmail.com>
2024-05-17 22:08:46 +0300

    Updated batch script to list files and directories

---
+++ list_files.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:: This is a Batch script to execute the 'dir' command
+
+:: Print a message
+echo Executing 'dir' command to list files and directories:
+
+:: Execute the 'dir' command
+dir
--- list_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This is a Bash script to execute the 'Is' command
-
-# Print a message
-echo "Executing 'Is' command to list files and directories:"
-
-# Execute the 'Is' command
-Is
